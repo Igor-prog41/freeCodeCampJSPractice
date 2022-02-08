1076cfccb39a89e9ada1978b9976bb24779301a5
Igor-prog41 <61401897+Igor-prog41@users.noreply.github.com>
2022-02-07 23:26:34 -0700

    Update task2.js
    80% Js base

--- task2.js
+++ task2.js
@@ -11,3 +11,25 @@ const testObj = {
 // Only change code below this line
 const playerNumber = 16;  // Change this line
 const player = testObj[playerNumber];   // Change this line
+
+
+
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line   (val) 
+  const lookup = {
+   "alpha": "Adams",    
+    "bravo": "Boston",
+   "charlie": "Chicago",
+   "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
